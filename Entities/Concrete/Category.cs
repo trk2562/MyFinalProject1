6316978edb06045abf5671920886b3641a1b7dc4
trk2562 <yilmaz.tarik@ogr.deu.edu.tr>
2022-07-21 04:49:12 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public int CategoryName { get; set; }
+    }
+}
