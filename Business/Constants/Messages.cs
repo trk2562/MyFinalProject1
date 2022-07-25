420587850e6af6d8054572385a4067f552cbee80
trk2562 <yilmaz.tarik@ogr.deu.edu.tr>
2022-07-25 05:24:12 +0300

    10.Ders

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    //Basit bir mesaj olduğu için sürekli new'lememek adına "static" ekledik.
+    //Dipnot: publicler PASCAl KEY gereği değişkenleri büyük harf ile başlar.
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        public static string MaintenanceTime = "Sistem bakımda.";
+        public static string ProductListed = "Ürünler listelendi.";
+    }
+}
