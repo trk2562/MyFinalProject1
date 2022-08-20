5d6c0c4c9eb4f46886eea121bc9b720cb1b707f4
Tarık Yılmaz <yilmaz.tarik@ogr.deu.edu.tr>
2022-08-20 03:14:17 +0300

    15.Ders

--- Business/Abstarct/IProductService.cs
+++ Business/Abstarct/IProductService.cs
@@ -16,6 +16,7 @@ namespace Business.Abstarct
         IDataResult<Product> GetById(int productId);
         IResult Add(Product product);
         IResult Update(Product product);
+        IResult AddTransactionalTest(Product product);
 
     }
 }
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -3,6 +3,9 @@ using Business.BusinessAspect.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Performance;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Business;
@@ -34,6 +37,7 @@ namespace Business.Concrete
         //Claim: Bu kullanıcının product.add veya admin claimlerinden birine sahip olması gerekiyor demektir.
         [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
         {
             //Business ile validation kodları genelde birbiri ile karıştırılabilir.
@@ -57,7 +61,7 @@ namespace Business.Concrete
             return new SuccessResult(Messages.ProductAdded);
             
         }
-
+        [CacheAspect]//Key-cache verilen isim,value
         public IDataResult<List<Product>> GetAll()
         {
             if (DateTime.Now.Hour==22)
@@ -71,7 +75,8 @@ namespace Business.Concrete
         {
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==id));
         }
-
+        [CacheAspect]
+        //[PerformanceAspect(5)]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
@@ -87,6 +92,8 @@ namespace Business.Concrete
             return new SuccessDataResult<List<ProductDetailsDto>>(_productDal.GetProductDetails());
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Update(Product product)
         {
             throw new NotImplementedException();
@@ -120,5 +127,11 @@ namespace Business.Concrete
             }
             return new SuccessResult();
         }
+
+        //[TransactionScopeAspect]
+        //public IResult AddTransactionalTest(Product product)
+        //{
+
+        //}
     }
 }
